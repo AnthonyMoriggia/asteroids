94c213e8dd20a961c30c793d3d3f13f58bdbb387
jabrone51 <antmoriggia@yahoo.com>
2025-05-15 17:44:31 -0400

    initial asteroids setup, player moves in environment

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-    print("Starting Asteroids!\n"
-    f"Screen width: {SCREEN_WIDTH}\n"
-    f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
+        #print(f"fps: {pygame.time.Clock.get_fps(clock)}")        
 
 
 if __name__ == "__main__":
